534a3fecaab1d1bf26ac91b130f5b82dc0722b77
luoshuaiqing <shuaiqin@usc.edu>
2020-04-03 19:09:52 -0700

    change structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './bootstrap/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '.')
   },
   module: {
     rules: [
