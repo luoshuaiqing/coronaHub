c7edf822ad6a1ed6425d50c38c8c9c911c6d359f
luoshuaiqing <shuaiqin@usc.edu>
2020-04-16 22:13:01 -0700

    update post_item's input

--- js/post_item.js
+++ js/post_item.js
@@ -1,10 +1,18 @@
 
 let lastSelectImg = null;
 $('.category-box .img-box').click(function() {
-    if(lastSelectImg)
+    if(lastSelectImg) {
         lastSelectImg.toggleClass('animate');
+        let checkBox = lastSelectImg.find(`input[type='checkbox']`);
+        checkBox.prop("checked", !checkBox.prop("checked"));
+    }
+        
     $(this).toggleClass('animate');
     lastSelectImg = $(this);
+
+    let checkBox = $(this).find(`input[type='checkbox']`);
+    checkBox.prop("checked", !checkBox.prop("checked"));
+    // console.log(checkBox.prop("checked"));
 });
 
 
